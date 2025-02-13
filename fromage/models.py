a19b98d6af0438eeb8035e9cba45cea3e5be7e42
alemartina23 <alemartina23@gmail.com>
2025-02-13 14:42:15 +0100

    Add generate_for_images_and_text_offline. The function is the same as generate_for_images_and_text but the call to get_image_from_url is removed so that the function returns images urls instead of images.

--- fromage/models.py
+++ fromage/models.py
@@ -789,6 +789,160 @@ class Fromage(nn.Module):
 
         return return_outputs
 
+    def generate_for_images_and_texts_offline(
+        self,
+        prompts: List,
+        num_words: int = 0,
+        ret_scale_factor: float = 1.0,
+        top_p: float = 1.0,
+        temperature: float = 0.0,
+        max_num_rets: int = 1,
+        max_img_per_ret: int = 1,
+    ):
+        """
+        Encode prompts into embeddings.
+
+        Args:
+          prompts: List of interleaved PIL.Image.Image and strings representing input to the model.
+          num_words: Maximum number of words to generate for. If num_words = 0, the model will run its forward pass and return the outputs.
+          ret_scale_factor: Proportion to scale [RET] token logits by. A higher value may increase the probability of the model generating [RET] outputs.
+          top_p: If set to < 1, the smallest set of tokens with highest probabilities that add up to top_p or higher are kept for generation.
+          temperature: Used to modulate logit distribution.
+          max_num_rets: Maximum number of images to return in one generation pass.
+          max_img_per_ret: Maximum number of images to return for each [RET] token.
+        Returns:
+          return_outputs: List consisting of either str or List[PIL.Image.Image] objects, representing image-text interleaved model outputs.
+        """
+        input_embs = []
+        input_ids = []
+        add_bos = True
+
+        for i, p in enumerate(prompts):
+            if type(p) == Image.Image:
+                # Encode as image.
+                pixel_values = utils.get_pixel_values_for_model(
+                    self.model.feature_extractor, p
+                )
+                pixel_values = pixel_values.to(
+                    device=self.model.logit_scale.device,
+                    dtype=self.model.logit_scale.dtype,
+                )
+                pixel_values = pixel_values[None, ...]
+
+                visual_embs = self.model.get_visual_embs(
+                    pixel_values, mode="captioning"
+                )  # (1, n_visual_tokens, D)
+                input_embs.append(visual_embs)
+            elif type(p) == str:
+                text_ids = self.model.tokenizer(
+                    p, add_special_tokens=True, return_tensors="pt"
+                ).input_ids.to(self.model.logit_scale.device)
+                if not add_bos:
+                    # Remove <bos> tag.
+                    text_ids = text_ids[:, 1:]
+                else:
+                    # Only add <bos> once.
+                    add_bos = False
+
+                text_embs = self.model.input_embeddings(text_ids)  # (1, T, D)
+                input_embs.append(text_embs)
+                input_ids.append(text_ids)
+            else:
+                raise ValueError(
+                    f"Input prompts should be either PIL.Image.Image or str types, got {type(p)} instead."
+                )
+        input_embs = torch.cat(input_embs, dim=1)
+        input_ids = torch.cat(input_ids, dim=1)
+
+        if num_words == 0:
+            generated_ids = input_ids
+            outputs = self.model.lm(
+                inputs_embeds=input_embs, use_cache=False, output_hidden_states=True
+            )
+            # Map outputs to embeddings, so we can retrieve embeddings from the [RET] tokens.
+            out = []
+            for x, fc in zip(
+                self.model.args.text_emb_layers, self.model.text_hidden_fcs
+            ):
+                out.append(fc(outputs.hidden_states[x]))
+            embeddings = torch.stack(out, dim=-1).sum(dim=-1)
+            embeddings = embeddings / embeddings.norm(
+                dim=-1, keepdim=True
+            )  # (N, T, 256)
+        elif num_words > 0:
+            generated_ids, generated_embeddings, _ = self.model.generate(
+                input_embs,
+                num_words,
+                temperature=temperature,
+                top_p=top_p,
+                ret_scale_factor=ret_scale_factor,
+            )
+            embeddings = generated_embeddings[-1][:, input_embs.shape[1] :]
+
+            # Truncate to newline.
+            newline_token_id = self.model.tokenizer(
+                "\n", add_special_tokens=False
+            ).input_ids[0]
+            trunc_idx = 0
+            for j in range(generated_ids.shape[1]):
+                if generated_ids[0, j] == newline_token_id:
+                    trunc_idx = j
+                    break
+            if trunc_idx > 0:
+                generated_ids = generated_ids[:, :trunc_idx]
+                embeddings = embeddings[:, :trunc_idx]
+        else:
+            raise ValueError
+
+        # Save outputs as an interleaved list.
+        return_outputs = []
+        # Find up to max_num_rets [RET] tokens, and their corresponding scores.
+        all_ret_idx = [
+            i
+            for i, x in enumerate(generated_ids[0, :] == self.model.retrieval_token_idx)
+            if x
+        ][:max_num_rets]
+        seen_image_idx = []  # Avoid showing the same image multiple times.
+
+        last_ret_idx = 0
+        if len(all_ret_idx) == 0:
+            # No [RET] tokens.
+            caption = self.model.tokenizer.batch_decode(
+                generated_ids, skip_special_tokens=True
+            )[0]
+            return_outputs.append(utils.truncate_caption(caption))
+        else:
+            for ret_idx in all_ret_idx:
+                ret_emb = embeddings[:, ret_idx, :]
+                scores = self.emb_matrix @ ret_emb.T
+
+                # Downweight seen images.
+                for seen_idx in seen_image_idx:
+                    scores[seen_idx, :] -= 1000
+
+                # Get the top max_img_per_ret + 3 (in case some fail) images for each image.
+                _, top_image_idx = scores.squeeze().topk(max_img_per_ret + 3)
+                image_outputs = []
+                for img_idx in top_image_idx:
+                    # Find the first image that does not error out.
+                    try:
+                        seen_image_idx.append(img_idx)
+                        img = self.path_array[img_idx]
+                        image_outputs.append(img)
+                        if len(image_outputs) == max_img_per_ret:
+                            break
+                    except UnidentifiedImageError:
+                        pass
+
+                caption = self.model.tokenizer.batch_decode(
+                    generated_ids[:, last_ret_idx:ret_idx], skip_special_tokens=True
+                )[0]
+                last_ret_idx = ret_idx + 1
+                return_outputs.append(utils.truncate_caption(caption) + " [RET]")
+                return_outputs.append(image_outputs)
+
+        return return_outputs
+
     def get_log_lik_scores(self, prompts: List):
         """
         Output the log likelihoods of the given interleaved prompts.
